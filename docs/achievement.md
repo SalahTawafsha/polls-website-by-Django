5a904cbe704905a0f77a984a1c8e57b6d98e71fe
Salah Tawafsha <1200339@student.birzeit.edu>
2023-08-24 17:10:35 +0300

    initial commit

---
+++ docs/achievement.md
@@ -0,0 +1,35 @@
+**Project Achievement Report**
+
+**Project Title:** Django Polls tutorial
+
+**Project Duration:** 22/8/2023 - 24/8/2023
+
+**Project Overview:**
+
+`	`This project is a tutorial for Django framework. It is a web application that can create polls and vote on them.
+
+**Objectives:**
+
+- Learn how to create a Django project and run server.
+- Learn Django models and how to use them.
+- Learn Django views and how to use them.
+- Learn Django templates and how to use them.
+- Learn Django forms, genaric forms and how to use them.
+- Learn Django testing and how to use it.
+- Learn Django static files and how to use them.
+- Learn Django admin and how to use it.
+- Learn Django debug-tool and how to use it.
+
+**Result:**
+
+`	`polls application that can create polls and vote on them.
+
+**Challenges:**
+
+- Need to learn Django framework.
+
+
+**What’s new:**
+
+- Learn Django framework and how to use it.
+- use python shell.
---
+++ solution/polls/tests.py
@@ -0,0 +1,147 @@
+""" testing for polls app """
+import datetime
+
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+
+from .models import Question
+
+
+def create_question(question_text, days):
+    """
+    Create a question with the given `question_text` and published the
+    given number of `days` offset to now (negative for questions published
+    in the past, positive for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionModelTests(TestCase):
+    """ Testing for question model. """
+
+    def test_was_published_recently_with_old_question(self):
+        """ test for was_published_recently method on old question."""
+        old_time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+        question = Question(pub_date=old_time)
+        self.assertIs(question.was_published_recently(), False)
+
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recintly_question(self):
+        """ test for was_published_recently method on recintly question."""
+        question = Question(pub_date=timezone.now())
+        self.assertIs(question.was_published_recently(), True)
+
+
+class QuestionIndexViewTests(TestCase):
+    """ Testing for question index view that is list of questions. """
+
+    def test_no_questions(self):
+        """
+        If no questions exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_past_question(self):
+        """
+        Questions with a pub_date in the past are displayed on the
+        index page.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_future_question(self):
+        """
+        Questions with a pub_date in the future aren't displayed on
+        the index page.
+        """
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
+
+    def test_future_question_and_past_question(self):
+        """
+        Even if both past and future questions exist, only past questions
+        are displayed.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question],
+        )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        question1 = create_question(question_text="Past question 1.", days=-30)
+        question2 = create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerySetEqual(
+            response.context["latest_question_list"],
+            [question2, question1],
+        )
+
+
+class QuestionDetailViewTests(TestCase):
+    """ Testing for question detail view
+    that is the form that we will vote in it."""
+
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question = create_question(question_text="Future question.", days=5)
+        url = reverse("polls:detail", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text="Past Question.", days=-5)
+        url = reverse("polls:detail", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
+
+
+# my added test cases
+class QuestionResultViewTests(TestCase):
+    """ Testing for question result view
+    that show number of votes for the poll. """
+
+    def test_feature_question(self):
+        """ test if feature question will not be avalible. """
+        feature_question = create_question("feature", days=5)
+        url = reverse("polls:results", args=(feature_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """ test if past question will be avalible. """
+        past_question = create_question("feature", days=-5)
+        url = reverse("polls:results", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
